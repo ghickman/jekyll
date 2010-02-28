b4607e7a8a1c1333823d15cb9d82d2faae1a22b0
Richard Grundy <rich@27smiles.com>
2010-02-28 16:06:23 +0000

    Added HAML support to the archive pages.

--- lib/jekyll/archive.rb
+++ lib/jekyll/archive.rb
@@ -7,19 +7,26 @@ module Jekyll
     #
     # Returns <Archive>
     def initialize(site, base, dir, type)
+      ext = File.extname(site.layouts['category_index'].name)
       @site = site
       @base = base
       @dir = dir
-      @name = 'index.html'
+      @name = "index#{ext}"
 
       self.process(@name)
 
-      self.read_yaml(File.join(base, '_layouts'), type + '.html')
+      self.read_yaml(File.join(base, '_layouts'), type + ext)
 
       year, month, day = dir.split('/')
       self.data['year'] = year.to_i
       month and self.data['month'] = month.to_i
       day and self.data['day'] = day.to_i
+
+      unless self.data['title'].nil?
+        self.data['title'].gsub!(/%year%/, year)
+        self.data['title'].gsub!(/%month%/, month) if month
+        self.data['title'].gsub!(/%day%/, day) if day
+      end
     end
   end
 
